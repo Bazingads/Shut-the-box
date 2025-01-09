bd87681704b82d73f1f075e97d00d76db21389c1
Bazingads <ethantmoto@gmail.com>
2025-01-09 02:37:52 -0500

    Update Die.java

--- MorimotoShutTheBox/src/morimoto/ethan/Die.java
+++ MorimotoShutTheBox/src/morimoto/ethan/Die.java
@@ -2,27 +2,47 @@ package morimoto.ethan;
 
 import java.util.Random;
 
+/**
+ * Purpose: Class simulating a 6 sided dice
+ * @author Ethan Morimoto
+ * @date January 8, 2025
+ */
 public class Die {
-	private int value;
-	private int numSides;
-	
-	Die (){
-		numSides = 6;
-		value = roll();
-	}
-	
-	Die(int n){
-		numSides = n;
-		value = roll();
-	}
-	
-	public int roll() {
-		Random rand = new Random();
-		value = rand.nextInt(1, numSides+1);
-		return value;
-	}
-	
-	public void setSides(int n) {
-		numSides = n;
-	}
+    private int value;    
+    private int numSides; 
+
+    /**
+     * Constructor without a specified number of sides.
+     */
+    public Die() {
+        numSides = 6;
+        value = roll();
+    }
+
+    /**
+     * Constructor with a specified number of sides.
+     * @param n The number of sides for the die
+     */
+    public Die(int n) {
+        numSides = n;
+        value = roll();
+    }
+
+    /**
+     * Rolls the die and returns a value from 1-6.
+     * @return value The value rolled
+     */
+    public int roll() {
+        Random rand = new Random();
+        value = rand.nextInt(1, numSides + 1);
+        return value;
+    }
+
+    /**
+     * Sets the number of sides on the die.
+     * @param n The new number of sides for the die
+     */
+    public void setSides(int n) {
+        numSides = n;
+    }
 }
